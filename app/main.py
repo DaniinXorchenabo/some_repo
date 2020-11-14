b16a2b7bd0f0e3b64c7be6353718c916f5f797db
DaniinXorchenabo <rkbcu@mail.ru>
2020-11-14 05:10:43 +0300

    add working with html-files

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
 from bottle import route, run, error, template
-
+from decorate_standart.decorate_standart_func import *
 
 @route('/hello')
 def hello():
     return "Hello World!"
 
+
 @route('/')
 def root_thie_site():
-    return template("view/main_page.html")
+    # return template('Здравствуй {{name}}, как дела?', name="Dfcz")
+    return temp("main_page", param="dfdf")
 
 
 @error(404)
 def error404(error):
     return 'Упс, извините страница не найдена!\n' + str(error)
+
 
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True)
